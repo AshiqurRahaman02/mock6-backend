e9da59c3aa14c4a4f62154c00bdb4ad05002b6b2
AshiqurRahaman02 <ashiqur999999@gamil.com>
2023-08-24 16:18:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const { conection } = require('./config/db')
+const { userRouter } = require('./routes/user.route')
+require("dotenv").config()
+
+const app = express()
+
+const port = process.env.port || 3000
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/user", userRouter)
+
+app.get('/',(req,res) => {
+    res.send("Welcome to the Express server")
+})
+
+app.listen(port , async () => {
+    try {
+        await conection
+        console.log("Connected with db")
+    } catch (error) {
+        console.log("Unable to connect with db")
+    }
+    console.log("App listening on port  " + port)
+})
---
+++ routes/user.route.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const { UserModel } = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken")
+
+require("dotenv").config()
+
+const userRouter = express.Router();
+
+userRouter.post('/api/register',async (req,res)=>{
+    try {
+        const {name,email,password} = req.body
+
+        bcrypt.hash(password, 2,async function(err, hash) {
+            if(err){
+                res.status(404).json({isError:true,message: err});
+            }
+
+            const newUser = new UserModel({email, password:hash, name})
+
+            await newUser.save();
+            res.status(201).json({isError:false,message: "User registered successfully"});
+        });
+    } catch (error) {
+        res.status(404).json({isError:true,message: error.message});
+    }
+})
+
+userRouter.post('/api/login',async (req,res)=>{
+    try {
+        const {email,password} = req.body
+        const user = await UserModel.findOne({email})
+        if(!user){
+            res.status(404).json({isError:true,message: "User not found"});
+        }
+
+        bcrypt.compare(password, user.password,async function(err, result) {
+            if(err){
+                res.status(501).json({isError:true,message: err});
+            }
+
+            res.status(201).json({isError:false,message: "User login successful", token:jwt.sign({userId: user._id},process.env.jtwSecret)});
+        });
+    } catch (error) {
+        res.status(404).json({isError:true,message: error.message});
+    }
+})
+
+module.exports = {
+    userRouter
+}

b160896d2c99213fc5b5f5ad22ecf0c6bd87e9ac
AshiqurRahaman02 <ashiqur999999@gamil.com>
2023-08-24 19:02:56 +0530

    fixing

--- routes/user.route.js
+++ routes/user.route.js
@@ -39,7 +39,7 @@ userRouter.post('/api/login',async (req,res)=>{
                 res.status(501).json({isError:true,message: err});
             }
 
-            res.status(201).json({isError:false,message: "User login successful", token:jwt.sign({userId: user._id},process.env.jtwSecret)});
+            res.status(201).json({isError:false,message: "User login successful",user, token:jwt.sign({userId: user._id},process.env.jtwSecret)});
         });
     } catch (error) {
         res.status(404).json({isError:true,message: error.message});
